85f98a69a71dfc4e567cad8bd11e2d4cc9f89d91
DDakQ <chhyun08ahn@gmail.com>
2025-11-17 08:59:37 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
 let factors;
 let chart;
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".cat-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+      const cat = btn.dataset.cat;
+      const target = document.getElementById("cat-" + cat);
+
+      target.classList.toggle("open");
+    });
+  });
+});  
+
 fetch("./data/emission_factors.json")
   .then(res => res.json())
   .then(data => {
     factors = data;
 
-    document.getElementById("calc-btn").addEventListener("click", () => {
-      const km = Number(document.getElementById("car-km").value);
+  document.getElementById("calc-btn").addEventListener("click", () => {
+    const inputs = document.querySelectorAll(".factor-input");
 
-      const result = km * factors.car;
+    const categorySums = {
+    move: 0,
+    food: 0,
+    device: 0
+  };
 
-      document.getElementById("result").textContent =
-        `오늘 탄소 배출량: ${result.toFixed(2)} kg CO2`;
+  inputs.forEach(input => {
+    const id = input.dataset.factorId;  
+    const category = id.replace(/[0-9]/g, ""); // move1 -> move
 
-      drawChart(result);
-    });
+    const value = Number(input.value);
+    const factor = factors[id];
+
+    if (!isNaN(value) && factor !== undefined) {
+      const emission = value * factor;
+      categorySums[category] += emission;
+    }
   });
 
-function drawChart(value) {
+    const total = categorySums.move + categorySums.food + categorySums.device;
+
+    document.getElementById("total-result").textContent =
+      `총 탄소 배출량: ${total.toFixed(2)} kg CO₂`;
+
+    drawChart(categorySums);
+  });
+});
+
+function drawChart(categorySums) {
   const ctx = document.getElementById("resultChart").getContext("2d");
 
   if (chart) {
@@ -26,16 +56,22 @@ function drawChart(value) {
   }
 
   chart = new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: ["탄소 배출량"],
-      datasets: [
+  type: "bar",
+  data: {
+    labels: ["이동", "음식", "전자기기 사용"],
+    datasets: [
         {
           label: "kg CO₂",
-          data: [value]
+          data: [
+            categorySums.move,
+            categorySums.food,
+            categorySums.device
+          ]
         }
       ]
     },
-    options: {}
+    options: {
+      indexAxis: "y" 
+    }
   });
 }
